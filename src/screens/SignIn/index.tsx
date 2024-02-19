43fdf116aef0e3b620f46044b763c2f4c3e0c907
macielribeiro <mac450@gmail.com>
2024-02-19 14:39:14 -0300

    adicionado tela splash

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export default function SignIn() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: 'red',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
