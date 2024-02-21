109a37c06af0727c17bd7e36bb8f56f863464b75
macielribeiro <mac450@gmail.com>
2024-02-21 10:12:49 -0300

    adicionado gradiente no background

--- App.tsx
+++ App.tsx
@@ -6,6 +6,7 @@ import {Rajdhani_500Medium, Rajdhani_700Bold} from '@expo-google-fonts/rajdhani'
 import AppLoading from "expo-app-loading";
 
 import SignIn from "./src/screens/SignIn";
+import { Background } from './src/componentes/Background';
 
 export default function App() {
     const [fontsLoaded] = useFonts({
@@ -20,13 +21,13 @@ export default function App() {
     };
 
  return (
-    <>
+    <Background>
         <StatusBar
         barStyle='light-content'
         backgroundColor='transparent'
         translucent
         />
         <SignIn />
-    </>
+    </Background>
  );
 }
